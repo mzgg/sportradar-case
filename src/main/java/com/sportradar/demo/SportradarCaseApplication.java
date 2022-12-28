7b2061e161e1f2ab6c26eff9aecdb01ca7d76c0f
Mehmet Zahit Guney <mehmetzahitt@gmail.com>
2022-12-28 12:46:53 +0300

    Start new Spring Boot 3.0.1 project

---
+++ src/main/java/com/sportradar/demo/SportradarCaseApplication.java
@@ -0,0 +1,13 @@
+package com.sportradar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportradarCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportradarCaseApplication.class, args);
+	}
+
+}

b2c01c7ff02b08ecc35f1bdc053c2bbd770e0da1
Mehmet Zahit Guney <mehmetzahitt@gmail.com>
2022-12-29 15:58:28 +0300

    Green: Update the match event test and prod code in a way that success

--- src/main/java/com/sportradar/demo/service/GameEventService.java
+++ src/main/java/com/sportradar/demo/service/GameEventService.java
@@ -2,15 +2,30 @@ package com.sportradar.demo.service;
 
 import com.sportradar.demo.model.Match;
 import com.sportradar.demo.resource.GameEventResource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class GameEventService {
 
+    private final MatchService matchService;
+
     public Match createGameEvent(GameEventResource gameEvent) {
-        return Match.builder()
+        Optional<Match> matchOptional = matchService
+                .findByHomeTeamNameAndAwayTeamNameAndStartDate(gameEvent.getHomeTeam(),
+                        gameEvent.getAwayTeam(), gameEvent.getMatchDate());
+
+        if (matchOptional.isPresent()){
+           return matchService.save(matchOptional.get());
+        }
+        Match match = Match.builder()
                 .homeTeamName(gameEvent.getHomeTeam())
                 .awayTeamName(gameEvent.getAwayTeam())
                 .build();
+
+        return matchService.save(match);
     }
 }
--- src/test/java/com/sportradar/demo/service/GameEventServiceTest.java
+++ src/test/java/com/sportradar/demo/service/GameEventServiceTest.java
@@ -3,11 +3,16 @@ package com.sportradar.demo.service;
 import com.sportradar.demo.model.Match;
 import com.sportradar.demo.resource.GameEventResource;
 
+import org.checkerframework.checker.nullness.Opt;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.swing.text.html.Option;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,7 +31,7 @@ public class GameEventServiceTest {
 
 
     @Test
-    void whenCalledCreateGameEvent_ItShouldSaveOrUpdateToDb() {
+    void whenCalledCreateGameEventIfNotExistInDb_ItShouldSaveToDb() {
         //given
         GameEventResource gameEvent = GameEventResource.builder()
                 .matchDate("1/2/2023 07:00")
@@ -35,14 +40,14 @@ public class GameEventServiceTest {
                 .awayTeam("Canada")
                 .build();
 
-        Match match = Match.builder()
+      Match match = Match.builder()
                 .matchStatus("NOT_STARTED")
                 .homeTeamName("Mexico")
                 .awayTeamName("Canada")
                 .build();
 
         //when
-        when(matchService.findByHomeTeamNameAndAwayTeamNameAndStartDate(any(), any(), any()));
+        when(matchService.findByHomeTeamNameAndAwayTeamNameAndStartDate(any(), any(), any())).thenReturn(Optional.empty());
         when(matchService.save(any())).thenReturn(match);
         Match savedMatch = gameEventService.createGameEvent(gameEvent);
 
@@ -51,7 +56,40 @@ public class GameEventServiceTest {
         verify(matchService).findByHomeTeamNameAndAwayTeamNameAndStartDate(any(), any(), any());
         verify(matchService).save(any());
 
+    }
 
+    @Test
+    void whenCalledCreateGameEventIfExistInDb_ItShouldUpdateToDb() {
+        //given
+        GameEventResource gameEvent = GameEventResource.builder()
+                .matchDate("1/2/2023 07:00")
+                .matchStatus("NOT_STARTED")
+                .homeTeam("Mexico")
+                .awayTeam("Canada")
+                .build();
+
+        Optional<Match> match = Optional.of(Match.builder()
+                .matchStatus("NOT_STARTED")
+                .homeTeamName("Mexico")
+                .awayTeamName("Canada")
+                .build());
+
+        Match updatedMatch = Match.builder()
+                .matchStatus("NOT_STARTED")
+                .homeTeamName("Mexico")
+                .awayTeamName("Canada")
+                .build();
+
+
+        //when
+        when(matchService.findByHomeTeamNameAndAwayTeamNameAndStartDate(any(), any(), any())).thenReturn(match);
+        when(matchService.save(any())).thenReturn(updatedMatch);
+        Match savedMatch = gameEventService.createGameEvent(gameEvent);
+
+        //then
+        then(savedMatch.getMatchName()).isEqualTo(gameEvent.getMatchName());
+        verify(matchService).findByHomeTeamNameAndAwayTeamNameAndStartDate(any(), any(), any());
+        verify(matchService).save(any());
 
     }
 }
